d718e8daac6b93efcb29218d46ec611dfd358d05
Khristine Santos <santos.348602@balagtas.sti.edu.ph>
2025-08-22 20:01:54 +0800

    Form design

--- CalculatorApplication/Form1.Designer.cs
+++ CalculatorApplication/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstNumber.Location = new System.Drawing.Point(12, 33);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(161, 20);
+            this.lblFirstNumber.TabIndex = 0;
+            this.lblFirstNumber.Text = "Enter First Number";
+            // 
+            // lblSecondNumber
+            // 
+            this.lblSecondNumber.AutoSize = true;
+            this.lblSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondNumber.Location = new System.Drawing.Point(12, 114);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(186, 20);
+            this.lblSecondNumber.TabIndex = 1;
+            this.lblSecondNumber.Text = "Enter Second Number";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(130, 181);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(68, 20);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput1.Location = new System.Drawing.Point(241, 33);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxInput1.TabIndex = 3;
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Location = new System.Drawing.Point(263, 65);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(57, 27);
+            this.cbOperator.TabIndex = 4;
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput2.Location = new System.Drawing.Point(241, 108);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxInput2.TabIndex = 5;
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayTotal.Location = new System.Drawing.Point(237, 181);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(0, 20);
+            this.lblDisplayTotal.TabIndex = 6;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEqual.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.Location = new System.Drawing.Point(233, 204);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(108, 36);
+            this.btnEqual.TabIndex = 7;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(372, 252);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblSecondNumber);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Name = "Form1";
+            this.Text = "FrmCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblSecondNumber;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.TextBox txtBoxInput2;
+        private System.Windows.Forms.Label lblDisplayTotal;
+        private System.Windows.Forms.Button btnEqual;
     }
 }
 
